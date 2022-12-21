53f5bf578bf1340e3dd41604043b7f5708aa442b
yapeansa <120826210+yapeansa@users.noreply.github.com>
2022-12-21 20:02:30 -0300

    Create primo.py

---
+++ primo.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+def isPrime(n):
+    test = True
+    if n == 0 or n == 1:
+        test = False
+    for i in range(2, n):
+        if n % i == 0:
+            test = False
+    return test
+
+while True:
+    print("Por favor, digite um número inteiro positivo: ")
+    n = int(input())
+    if n > 0: break
+
+if isPrime(n):
+    print("O número", n, "é primo.")
+else:
+    print("O número", n, "não é primo.")
